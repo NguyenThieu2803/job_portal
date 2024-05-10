82e08b85534d703846c628f7dbbb2f048523bddd
Nguyễn Văn Thiều <117129574+NguyenThieu2803@users.noreply.github.com>
2024-05-10 22:02:07 +0700

    Complete main 10/5/2024

--- Joblo-client/src/Pages/Home.jsx
+++ Joblo-client/src/Pages/Home.jsx
@@ -1,17 +1,74 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Banner from '../Components/Banner'
+import Card from '../Components/Card';
+import Jobs from './Jobs';
 
 const Home = () => {
+  const [selectedCategory, setSelectedCategory] = useState(null);
+  const [jobs, setJobs] = useState([]);
+  useEffect(() => {
+    fetch("jobs.json")
+      .then(res => res.json()).then(data => {
+        setJobs(data)
+      })
+  }, [])
+
   const [query, setQuery] = useState("");
-  const handleInputChange=(event)=>{
+
+  // search for
+  const handleInputChange = (event) => {
     setQuery(event.target.value);
   }
-  console.log(query);
+
+
+  //filter jobs by title
+  const filteredItem = jobs.filter((job) => job.jobTitle.toLowerCase().indexOf(query.toLowerCase()) !== -1);
+
+  //filter by radio button
+  const handleChange = (event) => {
+    setSelectedCategory(event.target.value);
+  }
+
+  //----button based filtering
+  const handleClick = (event) => {
+    selectedCategory(event.target.value);
+  }
+
+  // main function
+  const filterData = (query, selected, jobs) => {
+    let filteredJobs = jobs;
+
+//filter inputs
+    if (query) {
+      filteredJobs = filteredItem;
+    }
+
+    //filter by radio button
+    if(selected) {
+      filteredJobs= filteredJobs.filter(({jobLocation,maxPrice,experienceLevel,salaryType,employmentType,postingDate})=>{
+        jobLocation.toLowerCase() == selected.toLowerCase() ||
+        parseInt(maxPrice) == parseInt(selected) ||
+        experienceLevel.toLowerCase() == selected.toLowerCase() ||
+        salaryType.toLowerCase() == selected.toLowerCase() ||
+        employmentType.toLowerCase() == selected.toLowerCase() 
+      });
+      console.log(filteredJobs);
+    }
+    return filteredJobs.map((data,i)=><Card key={i} data ={data}/>)
+  }
+  const result = filterData(query,selectedCategory,jobs)
   return (
-    <div>
-    <Banner query={query} handleInputChange={handleInputChange}/>
+    <div >
+      <Banner query={query} handleInputChange={handleInputChange} />
+      <div className='bg-[#FAFAFA] md:grid grid-cols-4 gap-8 lg:px-24 px-4 py-12'>
+        <div className='bg-white p-4 rounded'>Left</div>
+      <div className='col-span-2 bg-white p-4 rounded-sm'><Jobs resuilt={result}/></div>
+      <div className='bg-white p-4 rounded'>Right</div>
+      </div>
+      
     </div>
   )
 }
+
 
 export default Home
---
+++ Joblo-client/src/Pages/Jobs.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Jobs = ({resuilt}) => {
+  return (
+    <div>{resuilt}</div>
+  )
+}
+
+export default Jobs
